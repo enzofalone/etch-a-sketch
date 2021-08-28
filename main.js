cebc9f0b4556b412d46b9802a319311cd87c3167
Enzo Falone <enzofalone123@gmail.com>
2021-08-28 02:31:16 -0400

    Update to main.js

--- main.js
+++ main.js
@@ -1,39 +1,27 @@
 let container = document.getElementById("main");
-container.style.maxWidth = "1024px";
 let cells = [];
 createGrid();
 
 function createGrid(cellSize) {
-    
-    for (let i = 0; i < (cellSize / 1024); i++) {
+    let x;
+    if(cellSize == null) {
+        x = 16;
+    } else {
+        x = cellSize;
+    }
+    for (let i = 0; i < x; i++) {
 
         let col = document.createElement("div");
-        //col.style.flex = "1 0 0%";
-        //col.style.margin = "0";
-        //col.style.alignContent = "flex-start";
-        //col.style.flexWrap = "wrap";
-        for (let j = 0; j < 48; j++) {
-            if(container.style.width >= "1024px") {
-                console.log("exceed");
-                break;
-            }
-            if(container.style.height >= "1024px") break;
-            
+
+        for (let j = 0; j < x; j++) {
 
             let cell = document.createElement("div");
 
             //style cell
-            if (!cellSize) {
-                cell.style.width = "16px";
-                cell.style.height = "16px";
-                cell.style.maxWidth = "16px";
-                cell.style.maxHeight = "16px";
-            } else {
-                cell.style.width = cellSize;
-                cell.style.height = cellSize;
-                cell.style.maxWidth = cellSize;
-                cell.style.maxHeight = cellSize;    
-            }
+            cell.style.width = "16px";
+            cell.style.height = "16px";
+            cell.style.maxWidth = "16px";
+            cell.style.maxHeight = "16px";
 
             cell.style.margin = "0";
             cell.style.boxSizing = "border-box";
@@ -41,38 +29,37 @@ function createGrid(cellSize) {
 
             //hover effect
             cell.addEventListener("mouseover", function () {
-                
+
                 if (!cell.classList.contains("hovered")) {
-                    this.style.backgroundColor = getRandomColor()
+                    this.style.backgroundColor = getRandomColor();
                     cell.classList.add("hovered");
                 }
             });
 
             //add to column
             col.appendChild(cell);
+            //add class to keep track
+            cell.classList.add("cell");
+            //add to cells array to track all elements
             cells.push(cell);
         }
         container.appendChild(col);
     }
-}
-
-function getRandomColor() {
-    return "rgb(" + (Math.random() * 255) + "," + (Math.random() * 255) + "," + (Math.random() * 255) + ")";
+    console.log("total cells = " + cells.length);
 }
 
 function resetGrid() {
-    //clean the grid
-    /*let drawnCells = document.querySelectorAll(".hovered");
-    for (let i = 0; i < drawnCells.length; i++) {
-        let drawnCell = drawnCells[i];
-        drawnCell.style.backgroundColor = "white";
-        drawnCell.classList.remove(".hovered");
-    }*/
-    for (let i = 0; i < cells.length; i++) {
-        let c = cells[i];
-        c.remove();
+    //clean the grid array
+    let totalCells = document.querySelectorAll(".cell");
+    for (let i = 0; i < totalCells.length; i++) {
+        totalCells[i].remove();
+        cells.splice(i, 1);
     }
+    let newSize = prompt("Input the amount of cells you want for the new grid");
+    createGrid(newSize);
+}
 
-    let newSize = prompt("Input the new size of the grid cells");
-    createGrid(newSize + "px");
+//optional feature
+function getRandomColor() {
+    return "rgb(" + (Math.random() * 255) + "," + (Math.random() * 255) + "," + (Math.random() * 255) + ")";
 }
